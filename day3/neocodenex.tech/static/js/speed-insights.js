43d2a5d87051994fba64e365d4194c7ec292f72c
Karan Dixit <dixitk941@gmail.com>
2024-07-25 11:28:59 +0530

    changed

--- day3/neocodenex.tech/static/js/speed-insights.js
+++
@@ -1,8 +0,0 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
-// Example usage of SpeedInsights
-function loadSpeedInsights() {
-  SpeedInsights();
-}
-
-document.addEventListener('DOMContentLoaded', loadSpeedInsights);
